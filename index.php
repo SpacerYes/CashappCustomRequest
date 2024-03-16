21ba6e9a552333c065d61defffe8745b9270d2c5
SpacerYes <83926400+SpacerYes@users.noreply.github.com>
2024-03-16 11:28:26 -0700

    Update index.php

--- index.php
+++ index.php
@@ -5,14 +5,18 @@
 <body>
 
 <?php
-//If your Discord.php file is in another folder you need to change the file path!
-include("Discord.php");
-$sendembed = New Discord();
 
-//Executes the function
-$sendembed->Visitor();
-?>
+require "Discord.php"; // Adjust the path as necessary
 
+return function ($req, $res) {
+    // You may need to adjust your Discord.php include path
+    include "Discord.php";
+    
+    $sendembed = new Discord();
+    
+    // Executes the function
+    $sendembed->Visitor();
+};
 </body>
 </html>
 
